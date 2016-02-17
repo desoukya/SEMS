34030ad4218e6c26ff99a082ea024bc6f5dc63c4
ali hassan <alithephantom@gmail.com>
2016-02-18 00:33:16 +0200

    Refactor FS collection Materials
    changed to a normal collection and a File Fs Collection

--- client/templates/schedule/scheduleEditor.js
+++ client/templates/schedule/scheduleEditor.js
@@ -1,19 +1,31 @@
 Template.scheduleEditor.events({
   'change .myFileInput': function(event, template) {
     FS.Utility.eachFile(event, function(file) {
-      Materials.insert(file, function(err, fileObj) {
+      Files.insert(file, function(err, fileObj) {
         if (err) {
           console.log(err);
           // handle error
         } else {
           // handle success depending what you need to do
-          var userId = Meteor.userId();
-          var imagesURL = {
-            "profile.image": "" + fileObj._id
+          
+          //TODO : should we add user data id
+          var material = {
+            fileId: fileObj._id,
+            title: event.target.title.value,
+            type: event.target.type.value,
+            week: event.target.week.value,
+            description: event.target.description.value,
+            createdAt: new Date() // current time
           };
-          console.log(imagesURL);
-          Meteor.users.update(userId, {
-            $set: imagesURL
+          console.log(material);
+          //TODO : should be replaced with meteor method
+          Materials.insert(material, function(error, result) {
+            if(error)
+            {
+              console.log(error.invalidKeys)
+            }
+            else
+              console.log(result);
           });
         }
       });
