328b966a2af9cd0ee6732978d41a8f7806b071ce
kady <elkadyabdelrahman@gmail.com>
2016-02-18 00:59:01 +0200

    Change user roles from admin panel

--- client/templates/admin/manageUsers.js
+++ client/templates/admin/manageUsers.js
@@ -4,6 +4,21 @@ Template.manageUsers.helpers({
   }
 });
 
+Template.manageUsers.events({
+  "click #update-users-button": function(event, template) {
+    var pairs = Session.get("toBeUpdatedRoles");
+
+    for (var id in pairs) {
+      if (pairs.hasOwnProperty(id)) {
+        Meteor.call("updateRole", id, pairs[id]);
+      }
+    }
+
+    Session.set("toBeUpdatedRoles", undefined);
+  },
+
+});
+
 
 Template.userEntry.rendered = function() {
   $('.ui.dropdown')
@@ -11,6 +26,26 @@ Template.userEntry.rendered = function() {
 
   Meteor.subscribe("users", Meteor.userId());
 };
+
+Template.userEntry.events({
+  "change .ui.selection.dropdown": function(event, template) {
+    var id = template.find(".ui.selection.dropdown").id;
+    var role = template.find("input[name=role]").value;
+
+    var roles = Session.get("toBeUpdatedRoles");
+    if (!roles) {
+      roles = {};
+    }
+
+    roles[id] = role;
+
+    Session.set("toBeUpdatedRoles", roles);
+
+  },
+
+
+
+});
 
 Template.userEntry.helpers({
   username: function() {
