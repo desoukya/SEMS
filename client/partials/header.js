44c131ad373fa7e4420b9eafe428338c7616f62c
kady <elkadyabdelrahman@gmail.com>
2016-02-19 20:55:08 +0200

    Highlight active item in navbar, Fixes #4

--- client/partials/header.js
+++ client/partials/header.js
@@ -2,5 +2,10 @@ Template.header.events({
   "click .logout": function(event) {
     Accounts.logout();
     Router.go('home');
+  },
+
+  "click .item": function(event, template) {
+    $(".item").removeClass("active");
+    $(event.target).addClass("active");
   }
 });
