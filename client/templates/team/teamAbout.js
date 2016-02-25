bc77f825c6f96353ed117d150d3367e3429bbc54
kady <elkadyabdelrahman@gmail.com>
2016-02-25 20:06:42 +0200

    Create teamAbout page with initial fake data

--- client/templates/team/teamAbout.js
+++ client/templates/team/teamAbout.js
@@ -4,4 +4,35 @@ Template.teamAbout.helpers({
     return this.name;
   },
 
+  members() {
+    return [{
+      name: "Kalinka Bash",
+      image: "/images/default_0.png",
+      tutorialGroup:"CSEN 10"
+    }, {
+      name: "Sue Namibia",
+      image: "/images/default_1.png",
+      tutorialGroup:"CSEN 15"
+    }, {
+      name: "Crash Bandicoot",
+      image: "/images/default_2.png",
+      tutorialGroup:"CSEN 11"
+    },
+    {
+     name: "Loop Indian",
+     image: "/images/default_3.png",
+     tutorialGroup:"CSEN 12"
+   },
+   {
+    name: "Crash Loop",
+    image: "/images/default_5.png",
+    tutorialGroup:"CSEN 12"
+  },
+  {
+   name: "Infinite thing",
+   image: "/images/default_4.png",
+   tutorialGroup:"CSEN 15"
+ }]
+  }
+
 });
