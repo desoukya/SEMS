8d82a89987d2b88479de272c722eefa9b86f8417
ali hassan <alithephantom@gmail.com>
2016-02-26 18:51:45 +0200

    Allow scrumMasters to add Users

--- client/templates/team/team.js
+++ client/templates/team/team.js
@@ -7,14 +7,31 @@ Template.team.helpers({
   teamImage() {
     return TeamUtils.getDefaultPhoto(this._id);
   },
-  users() {
-    console.log(Meteor.users.find({roles:'student'}).count());
-    return Meteor.users.find({roles:'student'});
+  members() {
+    return Meteor.users.find({
+      roles: 'student'
+    });
+  },
+  teamMembers() {
+    // TODO: Refactor to a methods ! 
+    var usersIds = Teams.findOne({
+      _id: this._id
+    }).members;
+
+    return Meteor.users.find({
+      _id: {
+        $in: usersIds
+      }
+    });
   }
 });
 Template.team.events({
   "click #addMembers": function(event) {
-    console.log($('.ui.search.dropdown').val());
+    var arr = $('#members').val().split(",");
+    for (var i =0; i < arr.length; i++) {
+      Teams.update({_id: this._id}, {$push : {members: arr[i]}}); 
+    };
+    
   },
 });
 
@@ -32,4 +49,24 @@ Template.userChoice.helpers({
   username() {
     return this.profile.firstName + " " + this.profile.lastName;
   },
+});
+
+Template.editableTeamMember.helpers({
+  image() {
+    var self = this;
+    var defaultPictureIndex = UserUtils.getDefaultPictureIndex(self._id);
+    return Images.findOne({
+      _id: self.profile.image
+    }) || {
+      url: `/images/default_${defaultPictureIndex}.png`
+    };
+  },
+
+  fullName() {
+    return this.profile.firstName + " " + this.profile.lastName;
+  },
+
+  tutorialGroup() {
+    return this.profile.tutorialGroup;
+  }
 });
