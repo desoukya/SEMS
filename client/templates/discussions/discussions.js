913dc9726ef158cb17ea0ed961c0ccf436015af1
ali hassan <alithephantom@gmail.com>
2016-03-06 18:17:24 +0200

    Allow users to add questions

---
+++ client/templates/discussions/discussions.js
@@ -0,0 +1,22 @@
+Template.discussions.events({
+    "submit .new-question": function(event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      var question = {
+        title: text,
+        askerId: Meteor.userId(),
+        createdAt: new Date() // current time
+      }
+      console.log(question);
+
+      // Insert a task into the collection
+      Questions.insert(question);
+
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
