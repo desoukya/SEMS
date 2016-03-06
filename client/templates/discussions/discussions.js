fe6e389e438af7947fa49b9511a56889060d72b1
ali hassan <alithephantom@gmail.com>
2016-03-06 18:47:57 +0200

    Add basic question template

--- client/templates/discussions/discussions.js
+++ client/templates/discussions/discussions.js
@@ -1,22 +1,27 @@
 Template.discussions.events({
-    "submit .new-question": function(event) {
-      // Prevent default browser form submit
-      event.preventDefault();
+  "submit .new-question": function(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
 
-      // Get value from form element
-      var text = event.target.text.value;
+    // Get value from form element
+    var text = event.target.text.value;
 
-      var question = {
-        title: text,
-        askerId: Meteor.userId(),
-        createdAt: new Date() // current time
-      }
-      console.log(question);
-
-      // Insert a task into the collection
-      Questions.insert(question);
-
-      // Clear form
-      event.target.text.value = "";
+    var question = {
+      title: text,
+      askerId: Meteor.userId(),
+      createdAt: new Date() // current time
     }
-  });
+    console.log(question);
+
+    // Insert a task into the collection
+    Questions.insert(question);
+
+    // Clear form
+    event.target.text.value = "";
+  }
+});
+Template.discussions.helpers({
+  questions() {
+    return Questions.find({},{'createdAt':-1});
+  },
+});
