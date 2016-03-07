410589a3c2ef4674c207d41de5fb5257a8d79bda
kady <elkadyabdelrahman@gmail.com>
2016-03-07 22:30:05 +0200

    Questions can be deleted, adding answers schema

--- client/templates/discussions/question/questionDetailed.js
+++ client/templates/discussions/question/questionDetailed.js
@@ -12,4 +12,38 @@ Template.questionDetailed.helpers({
     return moment(this.createdAt, 'ddd, MMM DD YYYY HH:mm:ss ZZ').format('LLL');
   },
 
+  canEdit() {
+    return this.ownerId === Meteor.userId() || Roles.userIsInRole(Meteor.userId(), [ADMIN, LECTURER, TA]);
+  }
+
+});
+
+Template.questionDetailed.events({
+  'click #delete-icon': function(event, template) {
+    event.preventDefault();
+    let self = this;
+
+    $('#delete-question-modal').modal({
+      closable: false,
+      onDeny() {
+
+      },
+      onApprove() {
+
+        let questionId = self._id;
+
+        // Delete this bad question and redirect
+        Meteor.call('deleteQuestion', questionId, function(err) {
+          if (err)
+            sAlert.error(err.reason);
+          else
+            Router.go('discussions');
+        });
+
+      }
+    }).modal('show');
+
+  },
+
+
 });
