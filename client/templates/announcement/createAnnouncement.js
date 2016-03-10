4f34ca974e67c533bc83e304d28af618ac162a9b
ali hassan <alithephantom@gmail.com>
2016-03-10 19:02:20 +0200

    Add createAnnouncements (backend)

--- client/templates/announcement/createAnnouncement.js
+++ client/templates/announcement/createAnnouncement.js
@@ -1,17 +1,31 @@
 Template.createAnnouncement.helpers({
-  teamCompanies() {
-
-    var takenCompanies = [];
-
-    Teams.find().fetch().forEach(function(team) {
-      takenCompanies.push(team.company);
-    });
-    var arr = Companies.find({ _id: { $in: takenCompanies } });
-    console.log(arr.fetch());
-    return arr;
+  teams() {
+    return Teams.find();
   },
 
 });
 Template.createAnnouncement.onRendered(function() {
   $('.ui.dropdown').dropdown();
 });
+
+Template.createAnnouncement.events({
+  'submit .form': function(event) {
+    event.preventDefault();
+
+    var title = event.target.title.value;
+    var description = event.target.description.value;
+    var companies = event.target.companies.value.split(',').filter(function(company) {
+      return company.length > 0;
+    });
+    var announcement = {
+      title: title,
+		  ownerId: Meteor.userId(),
+		  description: description,
+		  teams: companies,
+		  createdAt: new Date()
+		};
+		console.log(announcement);
+    Announcements.insert(announcement);
+
+  },
+})
