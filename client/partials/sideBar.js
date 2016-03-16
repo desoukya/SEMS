9d799517d4bccc5078f8006d2e979b9fbf55eaf2
ali hassan <alithephantom@gmail.com>
2016-03-16 03:14:55 +0200

    Rename team template to editTeam

--- client/partials/sideBar.js
+++ client/partials/sideBar.js
@@ -2,7 +2,6 @@ Template.sideBar.helpers({
 
   canEdit() {
   	var userId = Meteor.userId();
-  	console.log(userId, this._id);
   	if(!!userId && !!this._id)
     return TeamUtils.canEditTeam(userId,this._id);
   },
