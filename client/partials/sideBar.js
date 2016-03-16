02b6becf935ab31f656040bcf79f49162477d667
ali hassan <alithephantom@gmail.com>
2016-03-16 02:36:28 +0200

    Remove edit team option from sidebar unless admin/scrum

--- client/partials/sideBar.js
+++ client/partials/sideBar.js
@@ -1,8 +1,10 @@
-Template.header.helpers({
-  isActive(route) {
-    if (isCurrentRoute(route)) {
-      return 'active';
-    }
-    return '';
+Template.sideBar.helpers({
+
+  canEdit() {
+  	var userId = Meteor.userId();
+  	console.log(userId, this._id);
+  	if(!!userId && !!this._id)
+    return TeamUtils.canEditTeam(userId,this._id);
   },
+
 });
