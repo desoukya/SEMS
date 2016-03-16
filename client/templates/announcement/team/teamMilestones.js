1d08b3af46214c642ad3701accae92c3a36aec70
kady <elkadyabdelrahman@gmail.com>
2016-03-17 01:23:53 +0200

    Enable date meta popup in announcement

--- client/templates/announcement/team/teamMilestones.js
+++ client/templates/announcement/team/teamMilestones.js
@@ -1,9 +1,13 @@
+Template.teamMilestones.onRendered(function() {
+  $('.date.meta').popup({ inline: true });
+});
+
 Template.teamMilestones.helpers({
-    milestones() {
-      return Announcements.find({$or:[{global:true},{teams:this._id}]}, {
+  milestones() {
+    return Announcements.find({ $or: [{ global: true }, { teams: this._id }] }, {
       sort: {
         createdAt: -1
       }
     });
-},
+  },
 });
