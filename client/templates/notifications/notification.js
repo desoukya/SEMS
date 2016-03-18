1aa1196b0ac744bc27db7fcaca198a9899ec6090
ali hassan <alithephantom@gmail.com>
2016-03-18 21:31:28 +0200

    Limit notifcation  size in normal views

--- client/templates/notifications/notification.js
+++ client/templates/notifications/notification.js
@@ -5,3 +5,14 @@ Template.notification.events({
   },
 
 });
+
+Template.notification.helpers({
+  formattedContent() {
+  	
+  	if(this.content.length > 50)
+  		return this.content.substring(0,50)+"...";
+  	else
+  		return this.content;
+  },
+
+});
