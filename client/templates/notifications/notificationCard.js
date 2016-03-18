cfeb894d957ce3040fd76e8094a3fb68da84f069
kady <elkadyabdelrahman@gmail.com>
2016-03-18 03:05:09 +0200

    Send notification on annoucment addition

--- client/templates/notifications/notificationCard.js
+++ client/templates/notifications/notificationCard.js
@@ -1,6 +1,5 @@
 Template.notificationCard.events({
   'click #notification-card': function(event) {
-    event.preventDefault();
     Notifications.read(this._id);
   },
 
--- server/methods/announcements.js
+++ server/methods/announcements.js
@@ -2,10 +2,37 @@
 Meteor.methods({
   createAnnouncement(announcement) {
     // TODO: Check the arguments passed from client - Never Trusted !
+
+    let { title, ownerId, description, global, milestone, teams } = announcement;
+
     announcement.createdAt = Date.now();
-    if (Roles.userIsInRole(Meteor.userId(), [ADMIN, LECTURER, TA]))
-      return Announcements.insert(announcement);
-    else
+
+    if (Roles.userIsInRole(Meteor.userId(), [ADMIN, LECTURER, TA])) {
+      let announcementId = Announcements.insert(announcement);
+
+      teams.forEach(function(teamId) {
+
+        let team = Teams.findOne({ _id: teamId });
+
+        let members = team.members;
+        let link = milestone ? `/milestones/${announcementId}` : `/teams/${team.slug}/announcements`;
+        let typeHint = milestone ? 'Milestone :' : 'Announcement :';
+        let icon = milestone ? '<i class="idea icon"></i>' : '<i class="announcement icon"></i>';
+
+        members.forEach(function(id) {
+          Notifications.insert({
+            ownerId: id,
+            content: `${icon} ${typeHint} ${title}`,
+            link: link,
+            read: false,
+            createdAt: Date.now()
+          });
+        });
+
+      });
+
+      return announcementId;
+    } else
       throw new Meteor.Error(401, 'Not authorized to create an Announcement');
   },
 
