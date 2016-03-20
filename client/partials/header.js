0ecdc871546ede1d473623f516b7a8da94b514b0
kady <elkadyabdelrahman@gmail.com>
2016-03-20 20:40:38 +0200

    Set header color with meteor settings temporarily

--- client/partials/header.js
+++ client/partials/header.js
@@ -2,7 +2,7 @@
 Template.header.onRendered(function() {
 
   Meteor.call('getNodeEnv', function(err, env) {
-    if (env !== 'production' && Meteor.settings.environment !== 'production') {
+    if (env === 'development') {
       $('.ui.large.top.hidden.menu').addClass('teal inverted');
     }
   });
