12e27d431ee7db3b3826fb260a1d414a194b03e3
kady <elkadyabdelrahman@gmail.com>
2016-03-21 19:09:09 +0200

    Allow question down/up vote

---
+++ client/templates/discussions/questions.js
@@ -0,0 +1,50 @@
+Template.question.helpers({
+  activeUp() {
+    let question = Questions.findOne({ _id: this._id });
+
+    if (!question)
+      return
+
+    let upvoters = question.upvotes
+      .map(function(x) {
+        return x.ownerId;
+      });
+
+    return _.contains(upvoters, Meteor.userId()) ? 'green' : '';
+
+  },
+
+  activeDown() {
+    let question = Questions.findOne({ _id: this._id });
+
+    if (!question)
+      return
+
+    let downvoters = question.downvotes
+      .map(function(x) {
+        return x.ownerId;
+      });
+
+    return _.contains(downvoters, Meteor.userId()) ? 'red' : '';
+
+  },
+
+});
+
+Template.question.events({
+  'click #upvote': function(event) {
+    Meteor.call('upvoteQuestion', this._id, function(err) {
+      if (err)
+        sAlert.error(err.reason);
+    });
+
+  },
+
+  'click #downvote': function(event) {
+    Meteor.call('downvoteQuestion', this._id, function(err) {
+      if (err)
+        sAlert.error(err.reason);
+    });
+  }
+
+});
