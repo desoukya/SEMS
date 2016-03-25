337e9abf9bbf949a25d187f46b6449facd17b974
ali hassan <alithephantom@gmail.com>
2016-03-25 15:29:52 +0200

    Add week 9

--- client/templates/schedule/materialGroup.js
+++ client/templates/schedule/materialGroup.js
@@ -19,7 +19,7 @@ Template.materialGroup.helpers({
 
   },
   weekTitle(value) {
-    var weeks = ["Week 1","Week 2","Week 3","Week 4","Week 5","Week6","Midterms","Midterms","Week 8"];
+    var weeks = ["Week 1","Week 2","Week 3","Week 4","Week 5","Week6","Midterms","Midterms","Week 9"];
     //arrays are zero indexed ..  weeks are not
     return weeks[value-1];
   }
