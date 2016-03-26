20ad1a16cdb3411b8e5f15c23a75eee4b9ac26c2
omarkamal95 <omarkamal95@gmail.com>
2016-03-26 13:57:35 +0200

    deleted comments in metrics

--- server/startup/metrics.js
+++ server/startup/metrics.js
@@ -1,23 +1,4 @@
 //Calls function every 6 hours
-// Meteor.setInterval(function() {
-  // Meteor.call("calculateDailyLeaderBoard")
-// }, 1000 * 60 * 2)
-// 60 * 6
-
-// metrics = new Metrics();
-//       metrics.contributorsStatistics("https://github.com/desoukya/SEMS",function(err, res){
-//           if(err){
-//              // console.log("error hena ya 3am", err);
-//          }
-//          else{
-//              console.log(res.data);
-//          }
-//      });
-//      metrics.weeklyCodeFrequency("https://github.com/desoukya/SEMS",function(err, res){
-//          if(err){
-//              // console.log("error hena ya 3am", err);
-//          }
-//          else{
-//              console.log(res.data);
-//          }
-//      });
+Meteor.setInterval(function() {
+  Meteor.call("calculateDailyLeaderBoard")
+}, 1000 * 60 * 60 * 6)
