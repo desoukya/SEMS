65d550370e34d90d9348748b408f6482ba045c12
kady <elkadyabdelrahman@gmail.com>
2016-03-27 00:08:11 +0200

    Generalize addComment method to work with Questions and Answers

--- client/templates/discussions/comment/commentForm.js
+++ client/templates/discussions/comment/commentForm.js
@@ -2,14 +2,20 @@ Template.commentForm.events({
   'click #add-comment-button': function(event, template) {
     event.preventDefault();
 
-    let questionId = this._id;
+    let collectionId = this._id;
     let content = $('textarea[name=comment]').val();
 
-    let data = { questionId, content };
+    // Passing collection type to use for Questions or Answers
+    // TODO: Can be passed as 2nd parameter better ?
+    let collectionType = 'question';
+
+    let data = { collectionId, content, collectionType };
 
     Meteor.call('addComment', data, function(err) {
       if (err)
         sAlert.error(err.reason)
+      else
+        $('textarea[name=comment]').val('');
     });
 
 
