f5dd35db4a38f589d131347a1c7d941ca0844f68
kady <elkadyabdelrahman@gmail.com>
2016-03-27 17:29:29 +0200

    Allow editing comments

--- client/templates/discussions/comment/comment.js
+++ client/templates/discussions/comment/comment.js
@@ -12,15 +12,6 @@ Template.comment.helpers({
 
 
 Template.comment.events({
-  'click #edit-comment-icon': function(event, template) {
-    let parent = Template.parentData(1);
-    let collectionName = parent.collectionName();
-    let commentId = this._id;
-    let parentId = parent._id;
-
-
-  },
-
   'click #delete-comment-icon': function(event, template) {
     let parent = Template.parentData(1);
     let collectionName = parent.collectionName();
@@ -43,9 +34,40 @@ Template.comment.events({
       }
     }).modal('show');
 
-
-
-
   },
+
+  'click #edit-comment-icon': function(event, template) {
+    event.preventDefault();
+    let self = this;
+    Session.set('commentId', this._id);
+
+    Meteor.defer(function() {
+      $('#edit-comment-modal').modal({
+        closable: true,
+        onDeny() {
+
+        },
+
+        onApprove() {
+          let commentId = self._id;
+          let content = $("textarea[name=comment_content]").val();
+
+          let comment = { commentId, content };
+
+          Meteor.call('updateComment', comment, function(err) {
+            if (err)
+              sAlert.error(err.reason);
+          });
+
+        },
+
+        onHide() {
+          // Clear the session on modal hide
+          Session.set('commentId', undefined);
+        }
+
+      }).modal('show');
+    });
+  }
 
 });
