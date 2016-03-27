22e78becedde8e24cc3b321c9af629b3fab82963
kady <elkadyabdelrahman@gmail.com>
2016-03-27 19:32:51 +0200

    Fix popup target and data in question list

--- client/templates/discussions/question.js
+++ client/templates/discussions/question.js
@@ -1,6 +1,6 @@
 Template.question.onRendered(function() {
-  $('.question-owner-popup-activator').popup({
-    popup: '.special.popup',
+  this.$('.question-owner-popup-activator').popup({
+    popup: this.$('.special.popup'),
     hoverable: true,
     delay: {
       show: 300,
