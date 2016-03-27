b598b0aebab9f648c61d2d6ceccfbfb421a60ec0
kady <elkadyabdelrahman@gmail.com>
2016-03-27 15:50:56 +0200

    Allow deleting comments

--- client/templates/discussions/comment/comment.js
+++ client/templates/discussions/comment/comment.js
@@ -3,3 +3,43 @@ Template.comment.helpers({
     return Meteor.users.findOne({ _id: this.ownerId });
   }
 });
+
+
+Template.comment.events({
+  'click #edit-comment-icon': function(event, template) {
+    let parent = Template.parentData(1);
+    let collectionName = parent.collectionName();
+    let commentId = this._id;
+    let parentId = parent._id;
+
+
+  },
+
+  'click #delete-comment-icon': function(event, template) {
+    let parent = Template.parentData(1);
+    let collectionName = parent.collectionName();
+    let commentId = this._id;
+    let parentId = parent._id;
+
+    let data = { collectionName, commentId, parentId };
+
+    $('#delete-comment-modal').modal({
+      onDeny() {
+
+      },
+      onApprove() {
+
+        Meteor.call('deleteComment', data, function(err) {
+          if (err)
+            sAlert.error(err.reason);
+        });
+
+      }
+    }).modal('show');
+
+
+
+
+  },
+
+});
