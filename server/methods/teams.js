f17848bdae510e63454678cf383b61e08e44eace
omarkamal95 <omarkamal95@gmail.com>
2016-04-02 02:53:10 +0200

    fixed the week negative values and end of week restart bug

--- server/methods/teams.js
+++ server/methods/teams.js
@@ -88,7 +88,7 @@ Meteor.methods({
 
     Teams.find({}, { fields: { repo: 1, metrics: 1, members: 1 } }).forEach(function(team) {
       if (!team.metrics) {
-        Teams.update(team, { $push: { metrics: { totalWeeklyLines: 0, lineAdditions: 0, standardDev: 0, dailyPoints: 0, createdAt: Date.now() } } },
+        Teams.update(team, { $push: { metrics: { totalWeeklyLines: 0, lineAdditions: 0, standardDev: 0, dailyPoints: 0, createdAt: Date.now(), currentWeek: null } } },
           function(err, affected) {
             if (err) {
               console.log("error while updating".red, err)
@@ -102,7 +102,7 @@ Meteor.methods({
           if (!err && res.data) {
             if (res.data.length > 0) {
               additionsAndSubt = (res.data[res.data.length - 1][1]) + (-1 * res.data[res.data.length - 1][2]);
-
+              var latestWeek = res.data[res.data.length - 1][0];
               var teamMembers = [];
               if (team.members.length < 1) {
                 console.log("No members in team ".yellow + team.name);
@@ -118,13 +118,36 @@ Meteor.methods({
                       teamMembers.push(member);
                     }
 
-                    var lineDiff = additionsAndSubt - team.metrics[team.metrics.length - 1].totalWeeklyLines;
+                    if (team.metrics[team.metrics.length - 1].currentWeek != latestWeek) {
+                      var lineDiff = additionsAndSubt;
+                    } else {
+                      var lineDiff = additionsAndSubt - team.metrics[team.metrics.length - 1].totalWeeklyLines;
+                    }
+
                     var average = lineDiff / teamMembers.length;
                     var cumulative = 0;
                     for (var i = 0; i < teamMembers.length; i++) {
                       var userTotalWeeklyLines = 0;
                       var userLineAdditions = 0;
                       var found = false;
+
+                      if (!teamMembers[i].metrics) {
+                        Meteor.users.update(teamMembers[i], {
+                          $push: {
+                            metrics: {
+                              totalWeeklyLines: 0,
+                              lineAdditions: 0,
+                              createdAt: Date.now(),
+                              currentWeek: null
+                            }
+                          }
+                        }, function(err, affected) {
+                          if (err) {
+                            console.log("error while updating user with dummy basic metrics".red, err)
+                          }
+                        });
+                      }
+
                       for (var j = 0; j < contribStats.length; j++) {
                         if (teamMembers[i].profile.githubUser == contribStats[j].author.login) {
                           found = true;
@@ -132,8 +155,12 @@ Meteor.methods({
                           userTotalWeeklyLines = contribStats[j].weeks[contribStats[j].weeks.length - 1].a +
                             contribStats[j].weeks[contribStats[j].weeks.length - 1].c;
 
-                          userLineAdditions = userTotalWeeklyLines -
-                            teamMembers[i].metrics[teamMembers[i].metrics.length - 1].totalWeeklyLines
+                          if (teamMembers[i].metrics[teamMembers[i].metrics.length - 1].currentWeek != latestWeek) {
+                            userLineAdditions = userTotalWeeklyLines;
+                          } else {
+                            userLineAdditions = userTotalWeeklyLines -
+                              teamMembers[i].metrics[teamMembers[i].metrics.length - 1].totalWeeklyLines
+                          }
                           break;
                         }
                       }
@@ -146,7 +173,8 @@ Meteor.methods({
                           metrics: {
                             totalWeeklyLines: userTotalWeeklyLines,
                             lineAdditions: userLineAdditions,
-                            createdAt: Date.now()
+                            createdAt: Date.now(),
+                            currentWeek: latestWeek
                           }
                         }
                       }, function(err, affected) {
@@ -172,7 +200,8 @@ Meteor.methods({
                           lineAdditions: lineDiff,
                           standardDev: sd,
                           dailyPoints: updatedPoints,
-                          createdAt: Date.now()
+                          createdAt: Date.now(),
+                          currentWeek: latestWeek
                         }
                       }
                     }, function(err, affected) {
@@ -223,10 +252,5 @@ Meteor.methods({
       });
     }
   }
-
-
-
-
-
 
 });
