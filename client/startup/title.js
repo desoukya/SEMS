909c25e3fb340eb37988f707c11d9b38964066cc
kady <elkadyabdelrahman@gmail.com>
2016-04-02 16:16:15 +0200

    Display unread notifications count in title

--- client/startup/title.js
+++ client/startup/title.js
@@ -1,5 +1,11 @@
 Meteor.startup(function() {
   Tracker.autorun(function() {
-    document.title = Session.get('title') || 'SEMS';
+    let notifications = Notifications.find({ read: false }).fetch();
+    let title = Session.get('title') || 'SEMS';
+
+    if (notifications.length > 0)
+      title = `(${notifications.length}) ${title}`;
+
+    document.title = title;
   });
 });
