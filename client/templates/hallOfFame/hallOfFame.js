f3be90756e2cda93aac045f11461ff8a20855906
ali hassan <alithephantom@gmail.com>
2016-04-09 23:56:47 +0200

    Add 28- winners css

--- client/templates/hallOfFame/hallOfFame.js
+++ client/templates/hallOfFame/hallOfFame.js
@@ -1,3 +1,18 @@
 Template.hallOfFame.onRendered(function() {
   $('.ui.accordion').accordion();
 });
+
+Template.hallOfFame.helpers({
+  winningTeamMembers() {
+    return [
+    {name : 'nadine mansour', image:'/images/hall-of-fame/28-/teamWinner/nadin.jpg'},
+    {name : 'mariam fawzy', image:'/images/hall-of-fame/28-/teamWinner/mariam.jpg'},
+    {name : 'mohammed badr', image:'/images/hall-of-fame/28-/teamWinner/badr.jpg'},
+    {name : 'mohammed meloul', image:'/images/hall-of-fame/28-/teamWinner/melouk.jpg'},
+    {name : 'nadine mansour', image:'/images/hall-of-fame/28-/teamWinner/nadin.jpg'},
+    {name : 'nadine mansour', image:'/images/hall-of-fame/28-/teamWinner/nadin.jpg'},
+    {name : 'nadine mansour', image:'/images/hall-of-fame/28-/teamWinner/nadin.jpg'},
+    {name : 'nadine mansour', image:'/images/hall-of-fame/28-/teamWinner/nadin.jpg'},
+    ];
+  },
+});
