61a87f4dd9b88e4ac841b5c7b894b89d64fdb449
ali hassan <alithephantom@gmail.com>
2016-04-10 23:45:31 +0200

    Add remanining 28- winning team members

--- client/templates/hallOfFame/hallOfFame.js
+++ client/templates/hallOfFame/hallOfFame.js
@@ -9,10 +9,11 @@ Template.hallOfFame.helpers({
     {name : 'mariam fawzy', image:'/images/hall-of-fame/28-/teamWinner/mariam.jpg'},
     {name : 'mohammed badr', image:'/images/hall-of-fame/28-/teamWinner/badr.jpg'},
     {name : 'mohammed meloul', image:'/images/hall-of-fame/28-/teamWinner/melouk.jpg'},
-    {name : 'nadine mansour', image:'/images/hall-of-fame/28-/teamWinner/nadin.jpg'},
-    {name : 'nadine mansour', image:'/images/hall-of-fame/28-/teamWinner/nadin.jpg'},
-    {name : 'nadine mansour', image:'/images/hall-of-fame/28-/teamWinner/nadin.jpg'},
-    {name : 'nadine mansour', image:'/images/hall-of-fame/28-/teamWinner/nadin.jpg'},
+    {name : 'marwa regal', image:'/images/hall-of-fame/28-/teamWinner/marwa.jpg'},
+    {name : 'mohammed raouf', image:'/images/hall-of-fame/28-/teamWinner/rauof.jpg'},
+    {name : 'soha abu hamam', image:'/images/hall-of-fame/28-/teamWinner/soha.jpg'},
+    {name : 'ali soliman', image:'/images/hall-of-fame/28-/teamWinner/soliman.jpg'},
+    {name : 'anas ashraf', image:'/images/hall-of-fame/28-/teamWinner/anas.jpg'},
     ];
   },
 });
