2c0789976e21d46e9f3891c7ab12cc3f116b906b
kady <elkadyabdelrahman@gmail.com>
2016-04-15 21:42:13 +0200

    Display total and individual grades

--- client/templates/team/gradeBook/teamGrades.js
+++ client/templates/team/gradeBook/teamGrades.js
@@ -6,6 +6,9 @@ Template.teamGrades.helpers({
     });
 
     if (res) {
+
+      let marksArray = res.teamsGrades.grades[0].marks;
+
       // Just for table, getting some space for main points
       res.teamsGrades.criteria.map(function(criterion) {
         return _.extend(criterion, { rowSpan: criterion.checklist.length + 1 });
@@ -17,9 +20,18 @@ Template.teamGrades.helpers({
 
       res.teamsGrades.criteria.forEach(function(criterion) {
         criterion.checklist.map(function(point) {
-          return _.extend(point, { "grade": res.teamsGrades.grades[0].marks[count++] });
+          return _.extend(point, { "grade": marksArray[count++] });
         });
       });
+
+      // Individual grade is not available for admins
+      res.individualsGrade = "-";
+      if (!_.isEmpty(res.individualsGrades))
+        res.individualsGrade = res.individualsGrades[0].marks[0].score;
+
+
+      // Total grade for team
+      res.totalGrade = marksArray[marksArray.length - 1].score;
 
     }
 
