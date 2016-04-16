41111f33e54435fc3c7140b42b5fbb5c3f580eb9
kady <elkadyabdelrahman@gmail.com>
2016-04-16 15:19:41 +0200

    Send slack notifications on new announcements and sprints

--- server/methods/announcements.js
+++ server/methods/announcements.js
@@ -10,26 +10,27 @@ Meteor.methods({
     if (Roles.userIsInRole(Meteor.userId(), [ADMIN, LECTURER, TA])) {
       let announcementId = Announcements.insert(announcement);
 
-      if (global)
-        teams = Teams.find({}, {
-          _id: 1
-        }).fetch().map(function(team) {
+      if (global) {
+        teams = Teams.find({}, { _id: 1 }).fetch().map(function(team) {
           return team._id;
-        })
+        });
+      }
 
+      let typeHint = milestone ? 'Milestone' : 'Announcement';
+
+      // Send system notifications
       teams.forEach(function(teamId) {
 
         let team = Teams.findOne({ _id: teamId });
 
         let members = team.members;
         let link = milestone ? `/milestones/${announcementId}` : `/teams/${team.slug}/announcements`;
-        let typeHint = milestone ? 'Milestone :' : 'Announcement :';
         let icon = milestone ? '<i class="idea icon"></i>' : '<i class="announcement icon"></i>';
 
         members.forEach(function(id) {
           Notifications.insert({
             ownerId: id,
-            content: `${icon} ${typeHint} ${title}`,
+            content: `${icon} ${typeHint} : ${title}`,
             link: link,
             read: false,
             createdAt: Date.now()
@@ -37,6 +38,25 @@ Meteor.methods({
         });
 
       });
+
+      let color = milestone ? '#002fbe' : '#fbfa62';
+      let { slack } = require('../slack');
+
+      let message = {
+        text: `<!everyone> *New ${typeHint} is on the system*`,
+        attachments: [{
+          fallback: `<!everyone> *New ${typeHint} is on the system*`,
+          color: color,
+          fields: [{
+            title: `${typeHint} : ${title}`,
+            value: `Please check the system for new ${typeHint}`,
+            short: true
+          }]
+        }]
+
+      };
+
+      slack.send(message);
 
       return announcementId;
     } else
