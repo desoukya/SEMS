d62c9c789eebc6fd7939a586de47e6876d053fa2
ali hassan <alithephantom@gmail.com>
2016-04-19 00:36:29 +0200

    Edit 28- winners images

--- client/templates/hallOfFame/hallOfFame.js
+++ client/templates/hallOfFame/hallOfFame.js
@@ -8,7 +8,7 @@ Template.hallOfFame.helpers({
     {name : 'nadine mansour', image:'/images/hall-of-fame/28-/teamWinner/nadin.jpg'},
     {name : 'mariam fawzy', image:'/images/hall-of-fame/28-/teamWinner/mariam.jpg'},
     {name : 'mohammed badr', image:'/images/hall-of-fame/28-/teamWinner/badr.jpg'},
-    {name : 'mohammed meloul', image:'/images/hall-of-fame/28-/teamWinner/melouk.jpg'},
+    {name : 'mohammed melouk', image:'/images/hall-of-fame/28-/teamWinner/melouk.jpg'},
     {name : 'marwa regal', image:'/images/hall-of-fame/28-/teamWinner/marwa.jpg'},
     {name : 'mohammed raouf', image:'/images/hall-of-fame/28-/teamWinner/rauof.jpg'},
     {name : 'soha abu hamam', image:'/images/hall-of-fame/28-/teamWinner/soha.jpg'},
