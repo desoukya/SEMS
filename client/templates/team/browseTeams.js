cf5ae278825f4ce98fb3506af1fcd5bb2624bee1
kady <elkadyabdelrahman@gmail.com>
2016-04-26 21:19:58 +0200

    Copy ionic id on icon click in team browse

--- client/templates/team/browseTeams.js
+++ client/templates/team/browseTeams.js
@@ -1,6 +1,18 @@
+Template.browseTeam.onRendered(function () {
+  // Initialize clipboard.js
+  let ionicClipboardHandler = new Clipboard('#ionicCopyIcon');
+
+  let self = this;
+
+  ionicClipboardHandler.on('success', function (event) {
+    sAlert.success('Ionic id is Copied to clipboard');
+  });
+
+});
+
 Template.browseTeam.helpers({
   teams() {
-    return Teams.find({}, {sort: {createdAt: 1}});
+    return Teams.find({}, { sort: { createdAt: 1 } });
   },
 
 });
