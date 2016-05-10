d01d89d2491fc41c90c6d35ee3b9efd3af45a596
Omar Kamal <omarkamal95@gmail.com>
2016-05-10 23:27:38 +0200

    code fixes

--- server/startup/survey.js
+++ server/startup/survey.js
@@ -1,9 +1,6 @@
 Meteor.startup(function() {
   if (Survey.find().count() === 0) {
-    // Creating a random password
-    var pass = Random.id(20);
 
-    // Creating the great admin
      Survey.insert({online: false});
   }
 
