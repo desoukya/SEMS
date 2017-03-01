37bab87413d0a72eb64bf61ff0bbbf1fbeea98e3
YasmeenWafa <yasmeen_wafa@hotmail.com>
2017-03-01 22:32:54 +0200

    two types of tags course/topic

--- client/templates/admin/tagsEdit.js
+++ client/templates/admin/tagsEdit.js
@@ -1,26 +1,48 @@
+Template.tagsEdit.created =  function()
+{
+  //  TODO
+  this.type = new ReactiveVar("Topic");
+}
+
 Template.tagsEdit.helpers({
 
 viewTags: function (){
   return Tags.find({});
 }
-
 });
 Template.tagsEdit.events({
   'submit .form-register' : function(e){
     e.preventDefault();
 
     var tagName = e.target.tagName.value;
-  if(!Tags.findOne({"name" : tagName}))
-    Tags.insert({"name" : tagName})
-  else {
-    sAlert.error("This tag already exists");
-        }
-        e.target.tagName.value = '';
+     var course = $('#course').prop('checked');
+  
+  Meteor.call('createTag', tagName, course, function(err)
+{
+  if(err)
+  sAlert.error("This tag already exists");
+
+});
+e.target.tagName.value = '';
   },
 
-  'click #delete' : function()
+  'click #delete-icon' : function()
   {
 
-    Tags.remove(this._id);
-  }
+    Meteor.call('deleteTag',this._id, function(err){
+      if(err)
+      sAlert.error(err.reason);
+    } )
+
+  },
+
+  'change #course': function(event) {
+    console.log(event.currentTarget.checked);
+    if (event.currentTarget.checked)
+    $('#label').text("Course");
+  else
+
+  $('#label').text("Topic");
+
+  },
 })
