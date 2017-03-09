d2c479830e94b979b6c4067e40be7cee61039394
YasmeenWafa <yasmeen_wafa@hotmail.com>
2017-03-09 12:02:11 +0200

    now a user can unsubsribe to tags in his/her profile

--- client/templates/user/profile/profile.js
+++ client/templates/user/profile/profile.js
@@ -33,6 +33,40 @@ Template.profile.helpers({
     if (team) {
       return team.name;
     }
+  },
+  getSubscriptions()
+  {
+
+  return Meteor.user().subscriptions;
   }
 
 });
+
+Template.profiletag.events({
+
+  'click #deleteTag' : function(event)
+  {
+    var array =Meteor.user().subscriptions;
+    var string = this;
+
+var removed = false;
+    for(var i = 0; i<array.length; i++)
+    {
+      if(array[i]==string)
+      {
+        array.splice(i,1);
+        removed = true
+        break;
+      }
+    }
+    console.log(array)
+    console.log(removed)
+    Meteor.call('updateSubscriptions', array, function(err){
+      if(err)
+      sAlert.error(err.reason);
+    })
+  //console.log(Meteor.user().subscriptions)
+
+  //console.log(array.indexOf($('#tagName').text()))
+  }
+})
