2276b7d688f4de9bfdc7e20d38dd01933f631d26
YasmeenWafa <yasmeen_wafa@hotmail.com>
2017-03-20 14:54:01 +0200

    some amendments

--- server/migrations/migration6.js
+++ server/migrations/migration6.js
@@ -6,7 +6,20 @@ Migrations.add({
  questions = Questions.find({});
     questions.forEach(function(question){
     //  console.log(user);
-      Questions.update({_id: question._id},{$set: {closed: false}}, false, true)
+    var answers = question.answers;
+    var bestFound = false;
+    for(var i = 0; i<answers.length; i++){
+      if(answers[i].bestAnswer==true){
+        bestFound=true;
+        break;
+      }
+    }
+    if(bestFound==false){
+      Questions.update({_id: question._id},{$set: {closed: false}}, false, true)}
+      else {
+        Questions.update({_id: question._id},{$set: {closed: true}}, false, true)
+
+      }
     });
   },
   down : function(){
