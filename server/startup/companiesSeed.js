a4f57fe2e061972072d9d623395debb850bbcadc
YasmeenWafa <yasmeen_wafa@hotmail.com>
2017-04-10 21:41:36 +0200

    having now team names instead of companies in creating announcement

--- server/startup/companiesSeed.js
+++ server/startup/companiesSeed.js
@@ -1,16 +1,21 @@
 Meteor.startup(function() {
-  let companiesNames = ["angular","ionic","mongodb","socket-io",
-  "backbone","electron","jasmine","npm","stylus","bonsai","ember",
-  "javascript","passport","vuejs","bootstrap","grunt","jquery","polymer",
-  "webpack","bower","gulp","karma-logo","pugjs","yeoman","Browserify",
-  "handlebars","marionette","raphael","zurb","coffeescript","hapi",
-  "materialize","react","css3","html5","meteor-icon","sass"];
+	let companiesNames = ["angular", "ionic", "mongodb", "socket-io",
+		"backbone", "electron", "jasmine", "npm", "stylus", "bonsai", "ember",
+		"javascript", "passport", "vuejs", "bootstrap", "grunt", "jquery", "polymer",
+		"webpack", "bower", "gulp", "karma-logo", "pugjs", "yeoman", "Browserify",
+		"handlebars", "marionette", "raphael", "zurb", "coffeescript", "hapi",
+		"materialize", "react", "css3", "html5", "meteor-icon", "sass"
+	];
 
-  if (Companies.find().count() === 0) {
-    companiesNames.forEach((company,index)=>{
-      // let company = 
-      Companies.insert({ '_id': `${index}`, 'image': `${company}.png`, 'name': company });
-    })
-  }
+	if(Companies.find().count() === 0) {
+		companiesNames.forEach((company, index) => {
+			// let company =
+			Companies.insert({
+				'_id': `${index}`,
+				'image': `${company}.png`,
+				'name': company
+			});
+		})
+	}
 
 });
