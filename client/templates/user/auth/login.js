89ec455b2410d59bcc87447c165509b736008a8c
YasmeenWafa <yasmeen_wafa@hotmail.com>
2017-04-17 19:31:27 +0200

    added session time tracking

--- client/templates/user/auth/login.js
+++ client/templates/user/auth/login.js
@@ -1,62 +1,64 @@
 Template.login.events({
-  'submit .form': function(event) {
-    event.preventDefault();
+	'submit .form': function(event) {
+		event.preventDefault();
 
-    var email = event.target.email.value.trim();
-    var password = event.target.password.value;
+		var email = event.target.email.value.trim();
+		var password = event.target.password.value;
 
-    Meteor.loginWithPassword(email, password, function(err) {
-      if (err) {
-        sAlert.error(err.reason);
-      } else {
+		Meteor.loginWithPassword(email, password, function(err) {
+			if(err) {
+				sAlert.error(err.reason);
+			} else {
+				mixpanel.time_event('SessionTime');
 
-        /////////////// Analytics ///////////////
-        analytics.identify(Meteor.userId(), {
-          email: Meteor.user().email(),
-          name: Meteor.user().fullName(),
-          GUCId: Meteor.user().profile.GUCId,
-          tutorialGroup: Meteor.user().profile.tutorialGroup,
-          roles: Meteor.user().roles
-        });
-        /////////////// Analytics ///////////////
 
-        Router.go('/');
-      }
-    });
+				/////////////// Analytics ///////////////
+				analytics.identify(Meteor.userId(), {
+					email: Meteor.user().email(),
+					name: Meteor.user().fullName(),
+					GUCId: Meteor.user().profile.GUCId,
+					tutorialGroup: Meteor.user().profile.tutorialGroup,
+					roles: Meteor.user().roles
+				});
+				/////////////// Analytics ///////////////
 
-  },
+				Router.go('/');
+			}
+		});
+
+	},
 
 });
 
 Template.login.onRendered(function() {
-  $(document).ready(function() {
-    $('.ui.form').form({
-      fields: {
+	$(document).ready(function() {
+		$('.ui.form').form({
+			fields: {
 
-        email: {
-          identifier: 'email',
-          rules: [{
-            type: 'empty',
-            prompt: 'Please enter your e-mail'
-          }, {
-            type: 'email',
-            prompt: 'Please enter a valid e-mail'
-          }]
-        },
+				email: {
+					identifier: 'email',
+					rules: [{
+						type: 'empty',
+						prompt: 'Please enter your e-mail'
+					}, {
+						type: 'email',
+						prompt: 'Please enter a valid e-mail'
+					}]
+				},
 
-        password: {
-          identifier: 'password',
-          rules: [{
-            type: 'empty',
-            prompt: 'Please enter your password'
-          }, {
-            type: 'length[6]',
-            prompt: 'Your password must be at least 6 characters'
-          }]
-        },
+				password: {
+					identifier: 'password',
+					rules: [{
+						type: 'empty',
+						prompt: 'Please enter your password'
+					}, {
+						type: 'length[6]',
+						prompt: 'Your password must be at least 6 characters'
+					}]
+				},
 
-      }
-    });
-  });
+			}
+		});
+	});
 
 });
