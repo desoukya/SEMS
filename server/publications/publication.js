0c71f356f020579290eb0a5a6af7be405232dec5
YasmeenWafa <yasmeen_wafa@hotmail.com>
2017-04-18 00:54:02 +0200

    added image subscriptions to view images

--- server/publications/publication.js
+++ server/publications/publication.js
@@ -16,27 +16,27 @@ Meteor.publish('TeamUsers', function(teamSlug) {
 	var TeamUsers = Teams.findOne({
 		slug: teamSlug
 	}).members
-	return Meteor.users.find({
+	return [Meteor.users.find({
 		_id: {
 			$in: TeamUsers
 		}
-	});
+	}), Images.find({})];
 });
 
 Meteor.publish('usersBasic', function() {
-	return Meteor.users.find({}, {
+	return [Meteor.users.find({}, {
 		fields: {
 			profile: 1,
 			roles: 1
 		}
-	})
+	}), Images.find({})]
 })
 
 //for user profile
 Meteor.publish('usersSpecific', function(userId) {
-	return Meteor.users.find({
+	return [Meteor.users.find({
 		_id: userId
-	})
+	}), Images.find({})]
 })
 Meteor.publish('users', function(roles = ROLES) {
 
